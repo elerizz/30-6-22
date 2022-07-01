d5e99a61b911a3b659e2e477a5d6ee4d432bba3d
elerizz <eleonorarizzitello@gmail.com>
2022-07-01 18:22:04 +0200

    chore: firstinit

--- README.md
+++
@@ -1,5 +0,0 @@
-# Esercizio
-Sulla base dell'esercizio di oggi, sistemare la veste grafica (CSS) del progetto.
-
-# Avanzato
-Aggiungere la funzionalita di rimozione per ogni card, che una volta cliccata cancella la card in questione.
--- utils.js
+++ utils.js
@@ -3,54 +3,57 @@ const c = (el) => document.createElement(el);
 const q = (el) => document.querySelector(el);
 
 const createCard = (parent, imgLink, title, price) => {
-  const wrapperEl = c('div');
-  const titleEl = c('h3');
-  const priceEl = c('p');
-  const imgEl = c('img');
+  const wrapperEl = c("div");
+  const titleEl = c("h3");
+  const priceEl = c("p");
+  const imgEl = c("img");
 
-  wrapperEl.className = "wrapper"
-  titleEl.className = "title"
-  priceEl.className = "price"
-  imgEl.className = "img"
+  wrapperEl.className = "wrapper";
+  titleEl.className = "title";
+  priceEl.className = "price";
+  imgEl.className = "img";
 
-  imgEl.setAttribute('alt', title);
-  imgEl.setAttribute('src', imgLink)
+  imgEl.setAttribute("alt", title);
+  imgEl.setAttribute("src", imgLink);
 
-  titleEl.textContent = title
-  priceEl.textContent = price
+  titleEl.textContent = title;
+  priceEl.textContent = price;
 
   wrapperEl.append(imgEl, titleEl, priceEl);
   parent.appendChild(wrapperEl);
-}
+};
 
 const createFriendEl = (parent, name, photo) => {
-  const wrapper = c('div');
-  const img = c('img');
-  const par = c('p');
+  const wrapper = c("div");
+  const img = c("img");
+  const par = c("p");
 
-  wrapper.className = 'friendCard';
-  img.setAttribute('alt', name);
-  img.setAttribute('src', photo);
+  wrapper.className = "friendCard";
+  img.setAttribute("alt", name);
+  img.setAttribute("src", photo);
   par.textContent = name;
 
   wrapper.append(img, par);
   parent.appendChild(wrapper);
-}
+};
 
-const createMessageEl = (parent, text, sender, date) => {
-  const wrapper = c('div');
-  const textPar = c('p');
-  const senderPar = c('p');
-  const dataPar = c('p');
+const createMessageEl = (parent, id, text, sender, date, callback) => {
+  const wrapper = c("div");
+  const textPar = c("p");
+  const senderPar = c("p");
+  const dataPar = c("p");
 
-  wrapper.className = 'messageCard';
+  wrapper.className = "messageCard";
   textPar.textContent = text;
   senderPar.textContent = sender;
+  wrapper.setAttribute("id", id);
   // dataPar.textContent = date.split('T')[0].split('-').reverse().join('-');
   dataPar.textContent = date;
 
+  wrapper.addEventListener("click", callback);
+
   wrapper.append(textPar, senderPar, dataPar);
   parent.appendChild(wrapper);
-}
+};
 
 export { c, q, createCard, createFriendEl, createMessageEl };
